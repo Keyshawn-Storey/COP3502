6a152cbd513ee4d93699191cdb7b0c8a722a6ff0
Keyshawn L Storey <kstorey@thunder.cise.ufl.edu>
2019-12-03 13:49:00 -0500

    first commit

---
+++ Cow.java
@@ -0,0 +1,23 @@
+class Cow {
+
+    private String name;
+    private String image;
+
+    public Cow(String name) {
+        this.name = name;
+    }
+    // Gets the name
+    public String getName() {
+        return name;
+    }
+    // Gets the image
+    public String getImage() {
+        return image;
+    }
+    // Sets the image
+    public void setImage(String _image) {
+        image = _image;
+    }
+}
+
+
